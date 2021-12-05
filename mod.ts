225650811851b3b620e64e7fc655d6a3fbfcd73e
ganariya <ganariya2525@gmail.com>
2021-12-05 21:25:46 +0900

    simple change

--- mod.ts
+++ mod.ts
@@ -1,4 +1,3 @@
 export type { GlitchOption, ConstructOption } from "./type.ts";
-// export type { GlitchOption, ConstructOption };
 export { useGlitch } from "./useGlitch.tsx";
 export { useConstruct } from "./useConstruct.tsx";
